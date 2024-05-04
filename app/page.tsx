15673131659e8d18133d1896ea069715dec59e43
SonnySanga <rohit.kumar018219>
2024-05-04 17:55:56 +0530

    installed shadcn and tested

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button"
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Coding </h1>
+    <main className="">
+           <Button>Hello Coder</Button>
+
           </main>
   );
 }
