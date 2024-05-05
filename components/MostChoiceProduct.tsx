f6a70e65a38f23ac6b2bbb2454ca0673a4bcd445
SonnySanga <rohit.kumar018219>
2024-05-05 11:08:17 +0530

    added popular product functionality

--- components/MostChoiceProduct.tsx
+++ components/MostChoiceProduct.tsx
@@ -4,7 +4,7 @@ import ProductCard from "./ProductCard";
 
 interface Product {
     id: number;
-    name: string;
+    title: string;
     description: string;
     image: string;
     price: number;
@@ -15,10 +15,10 @@ function MostChoiceProduct() {
     const [products, setProducts] = useState<Product[]>([]);
     const [isLoading, setLoading] = useState(true);
   useEffect(() => {
-    fetch("/productdata.json")
+    fetch("https://fakestoreapi.com/products")
       .then((res) => res.json())
       .then((data) => {
-        setProducts(data);
+        setProducts(data.slice(0,6));
         setLoading(false);
       });
   }, []);
@@ -28,11 +28,11 @@ function MostChoiceProduct() {
 
   return (
     <div>
-      <h2 className="font-medium text-sm p-1">
+      <h2 className="font-medium text-sm p-1 text-center">
         All the recent best liked product
       </h2>
       
-      <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
+      <div className="container mx-auto grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-8">
       {products.map(product => (
         <ProductCard key={product.id} product={product} />
       ))}
--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,8 +1,10 @@
 // components/ProductCard.tsx
 
+import { Button } from "./ui/button";
+
 interface Product {
     id: number;
-    name: string;
+    title: string;
     description: string;
     image: string;
     price: number;
@@ -15,13 +17,13 @@ interface Product {
   const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
     return (
       <div className="max-w-xs bg-white shadow-lg rounded-lg overflow-hidden">
-        <img className="w-full h-48 object-cover object-center" src={product.image} alt={product.name} />
+        <img className="w-32 h-32 object-cover object-center" src={product.image} alt={product.title} />
         <div className="p-4">
-          <h2 className="text-gray-800 text-lg font-semibold">{product.name}</h2>
-          <p className="mt-2 text-gray-600">{product.description}</p>
-          <div className="mt-4 flex items-center justify-between">
-            <span className="text-gray-900">${product.price}</span>
-            <button className="px-3 py-1 bg-gray-800 text-white text-xs font-semibold rounded uppercase hover:bg-gray-700 focus:bg-gray-700 focus:outline-none">Add to Cart</button>
+          <h2 className="text-gray-800  font-serif text-sm ">{product.title}</h2>
+          <p className="mt-2 text-gray-600 line-clamp-2">{product.description}</p>
+          <div className="mt-4 flex items-center justify-between space-x-2">
+            <span className="text-gray-900 text-sm">${product.price}</span>
+            <Button variant="outline" className="px-3  py-1 bg-gray-800 text-white text-xs font-semibold rounded uppercase hover:bg-gray-700 focus:bg-gray-700 focus:outline-none">Add to Cart</Button>
           </div>
         </div>
       </div>
